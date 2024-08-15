34a8f14e8d51c6f207b9c13a2c5b0317ab50cbc4
TOKHIRJON TURGUNOV <myfuzei@gmail.com>
2024-08-15 15:06:55 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 
 console.log("Web serverni boshlash");
 const express =  require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
 
 
 // 1 kirish code
@@ -11,10 +24,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
+
 // 2: Session code
 // 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs"); // backend ichida html front end yasaymiz
+
 
 
 // 4 Routing Code
@@ -22,6 +37,10 @@ app.post("/create-item", (req, res) => {
     console.log(req);     // post bu DB-ga malumot olip keladi
     res.json({test: "success"});
 });
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+})
 
 app.get('/', function(req, res) {
     res.render("harid");  // get DB-dan malumotni olish va uqish uchun 
@@ -42,7 +61,7 @@ app.get('/', function(req, res) {
 
 
 const server = http.createServer(app);
-let PORT = 3001;
+let PORT = 3000;
 server.listen(PORT, function(){
     console.log(`The server is Running successfully on port: ${PORT}`);
 });
