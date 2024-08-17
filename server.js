295c68907121082d9acdc717164a3a9095d09b9f
TOKHIRJON TURGUNOV <myfuzei@gmail.com>
2024-08-17 21:52:36 +0900

    feat: develop portfolio author page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 console.log("Web serverni boshlash");
-const express =  require("express");
+const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
@@ -17,12 +17,10 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 });
 
 
-
 // 1 kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 
 
 // 2: Session code
@@ -31,33 +29,21 @@ app.set("views", "views");
 app.set("view engine", "ejs"); // backend ichida html front end yasaymiz
 
 
-
 // 4 Routing Code
 app.post("/create-item", (req, res) => {
     console.log(req);     // post bu DB-ga malumot olip keladi
     res.json({test: "success"});
 });
 
+
+
 app.get('/author', (req, res) => {
     res.render("author", {user: user});
-})
+});
 
 app.get('/', function(req, res) {
     res.render("harid");  // get DB-dan malumotni olish va uqish uchun 
 });
-
-
-// app.get("/hello", function(req, res) {
-//     res.end('<h1 style="background: red"> Hello World! by BekzodAli</h1>');
-// });
-
-// app.get("/gift", function(req, res) {
-//     res.end('<h1> Siz sovgalar bolimidasiz! </h1>');
-// });
-
-// app.get("/blog", function(req, res) {
-//     res.end('<h1 style="background: green"> Siz Blog bolimidasiz! </h1>');
-// });
 
 
 const server = http.createServer(app);
