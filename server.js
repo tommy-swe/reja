a9a928f169310c3cb3f64b774174100b77e503dc
TOKHIRJON TURGUNOV <myfuzei@gmail.com>
2024-08-23 16:47:43 +0900

    fix: refactor

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.get('/author', (req, res) => {
 });
 
 app.get('/', function(req, res) {
-    res.render("harid");  // get DB-dan malumotni olish va uqish uchun 
+    res.render("reja");  // get DB-dan malumotni olish va uqish uchun 
 });
 
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function(){
-    console.log(`The server is Running successfully on port: ${PORT}`);
+    console.log(`The server is Running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
 
 
