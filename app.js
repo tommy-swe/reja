7fbed1a68527c839bdeaf9794e10f0169639f9ed
TOKHIRJON TURGUNOV <myfuzei@gmail.com>
2024-08-26 18:36:09 +0900

    feat: FrontEnd JavaScriptni qurish

--- app.js
+++ app.js
@@ -40,16 +40,19 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);     // post bu DB-ga malumot olip keladi
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("something went wrong!");
-        } else {
-            res.end('Successfully added');
-        }
+        console.log(data.ops);
+        res.json(data.ops[0]);
+        // if(err) {
+        //     console.log(err);
+        //     res.end("something went wrong!");
+        // } else {
+        //     res.end('Successfully added');
+        // }
     });
 });
     // res.end("success");
 //    res.json({test: "success"});
+
 
 
 app.get('/author', (req, res) => {
